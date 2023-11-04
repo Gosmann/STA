7b20986210e808a08fd857c9d0cf0e33837e21e7
Gabriel Gosmann <gabriel.gosmann@gmail.com>
2023-11-04 04:00:13 +0100

    get speed_data

--- arduino/model_experiments/model_experiments/model_experiments.ino
+++ arduino/model_experiments/model_experiments/model_experiments.ino
@@ -3,12 +3,28 @@ char buffer[100] = {0};
 
 float time_counter = 0;
 float voltage = 0;
+
 int32_t encoder_counter = 0;
+int32_t old_encoder_counter = 0;
+int32_t delta = 0;
+
 float omega = 0;
 
 char buffer_time[15] = {0};
 char buffer_voltage[15] = {0};
 char buffer_encoder[15] = {0};
+
+// pins used for encoder 1
+#define PORT1_NE1 31	// determines orientation B
+#define PORT1_NE2 18	// accepts interrupts A
+
+// pins used for encoder 2
+#define PORT2_NE1 38	// determines orientation B
+#define PORT2_NE2 19	// accepts interrupts A
+
+// defines quadrature encoder pins
+const byte A_Pin = PORT1_NE2;
+const byte B_Pin = PORT1_NE1;
 
 // pins used by the motor drivers
 #define PWMB_1 12
@@ -57,8 +73,8 @@ motor_t init_motor( uint8_t pwm, uint8_t forward, uint8_t backward ){
 // function to apply voltage to the motors
 int drive_voltage( motor_t motor, float voltage ){
 
-  if( voltage > 9.0 ) voltage = 9.0 ;
-  else if(voltage < -9.0) voltage = -9.0 ;
+  if( voltage > 12.0 ) voltage = 12.0 ;
+  else if(voltage < -12.0) voltage = -12.0 ;
     
   int pwm_value = abs(voltage) * VOLT_TO_PWM ;
 
@@ -89,6 +105,11 @@ void setup() {
 	drive_voltage( powertrain, 0.0);
   	drive_voltage( direction, 0.0);
 
+	pinMode(A_Pin, INPUT_PULLUP);
+	pinMode(B_Pin, INPUT_PULLUP);
+	
+	attachInterrupt(digitalPinToInterrupt(A_Pin), encoder_isr, RISING);
+	
 	cli();//stop interrupts
 
 	//set timer4 interrupt at 1Hz
@@ -98,6 +119,10 @@ void setup() {
 
 	// set compare match register for 10 ms increments
 	OCR4A = 624;				// = (16*10^6) / (256) - 1 (must be <65536)
+
+	// set compare match register for 50 ms increments
+	//OCR4A = 3124;				// = (16*10^6) / (256) - 1 (must be <65536)
+	
 	
 	// set compare match register for 1hz increments
 	//OCR4A = 62499;				// = (16*10^6) / (256) - 1 (must be <65536)
@@ -112,7 +137,7 @@ void setup() {
 	sei();//allow interrupts
 	
 	// print header
-	sprintf(buffer, "time [ms], voltage [V], encoders [pulses] \n");
+	sprintf(buffer, "time [ms], voltage [V], speed [rad/s] \n");
     Serial2.print(buffer);  
 	
 	dtostrf( time_counter, 5, 3, buffer_time);
@@ -140,13 +165,13 @@ ISR(TIMER4_COMPA_vect){
   		voltage = 0;
   	}
   	else if( time_counter < 6.0 ){
-  		voltage = 5;
+  		voltage = 12;
   	}
   	else if( time_counter < 9.0 ){
   		voltage = 0;
   	}
   	else if( time_counter < 12.0 ){
-  		voltage = -5;
+  		voltage = -12;
   	}
   	else if( time_counter < 15.0){
   		voltage = 0;
@@ -162,17 +187,37 @@ ISR(TIMER4_COMPA_vect){
 	else led_state = 1;
 	digitalWrite(13, led_state);
 
+	delta = encoder_counter - old_encoder_counter ;
+	old_encoder_counter = encoder_counter ;
+
+	omega = (delta * 0.243912625 );		// converts from delta encoder to omega
+	// supposes delta_t = 10ms
+	// and gear ratio 2576
+	
 	dtostrf( time_counter, 6, 3, buffer_time);
 	dtostrf( voltage, 6, 3, buffer_voltage);
-	dtostrf( encoder_counter, 6, 3, buffer_encoder);
+	dtostrf( omega, 6, 3, buffer_encoder);
 	sprintf(buffer, "%s, %s, %s \n", buffer_time, buffer_voltage, buffer_encoder );
 	
-  	if( time_counter <= 15.0 ){
+  	if( time_counter < 15.0 ){
   		Serial2.print(buffer);
   	}
   	else{
-  		Serial.end();
+  		Serial2.print("end \n");
+  		//Serial.end();
   		Serial2.end();
   	}
 
+}
+
+void encoder_isr() {
+	if( digitalRead( B_Pin ) ){		// checks for polarity
+		encoder_counter++;	
+		
+	}
+	else{
+		encoder_counter--;	
+	}
+
+	
 }
