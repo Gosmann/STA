b1dce09502eba5f1c2974aeac726542e566381a2
Gabriel Gosmann <gabriel.gosmann@gmail.com>
2023-11-05 01:19:24 +0100

    create pos_control file

---
+++ arduino/control/pos_PI/encoders.ino
@@ -0,0 +1,48 @@
+//#include "encoders.hpp"
+
+// function to initialize the encoders
+encoder_t init_encoder( uint8_t A, uint8_t B ){
+
+    encoder_t encoder = { A, B, 0, 0, 0} ;
+    
+    pinMode(encoder.A, INPUT_PULLUP);
+    pinMode(encoder.B, INPUT_PULLUP);
+
+    return encoder;
+}
+
+// calculates encoder speed
+void calculate_speed( encoder_t * encoder ){
+	
+    int32_t delta = encoder->odom - encoder->old_odom ;
+	
+    // converts from delta encoder to omega [rad/s]
+    encoder->omega = ( (float)delta * 0.243912625 );         
+    // supposes delta_t = 10ms
+    // and gear ratio 2576
+    
+    encoder->old_odom = encoder->odom;
+}
+
+// treats encoder interrupt service routine
+void power_isr() {
+    //Serial2.println(" power isr ");
+    
+    if( digitalRead( power.B ) ){     // checks for polarity
+        power.odom++;
+    }
+    else{
+        power.odom--;  
+    }
+}
+
+// treats encoder interrupt service routine
+void direc_isr() {
+    
+    if( digitalRead( direc.B ) ){     // checks for polarity
+        direc.odom++;
+    }
+    else{
+        direc.odom--;  
+    }
+}
---
+++ arduino/control/pos_PI/pos_PI.ino
@@ -0,0 +1,102 @@
+#include "encoders.hpp"
+#include "interrupts.hpp"
+#include "motors.hpp"
+
+char buffer[100] = {0};
+
+static motor_t powertrain = init_motor( 12, 34, 35 );
+static motor_t direction = init_motor( 8, 37, 36 );
+
+static encoder_t power = init_encoder( 18, 31 );
+static encoder_t direc = init_encoder( 19, 38 );
+
+static float set_point = 0 ;
+
+// put your setup code here, to run once:
+void setup() {
+	
+  	//Serial.begin(115200);         // /dev/ttyUSB0   usb cable serial
+  	Serial2.begin(115200);          // /dev/ttyS0     gpios on rpi
+	
+	pinMode(13, OUTPUT);            // blue LED
+
+    
+    start_encoders_interrupt();
+    start_timer_interrupt();        // starts 10ms timer interrupt
+    
+	// print header
+	sprintf(buffer, "Hello World! \n");
+    Serial2.print(buffer);  
+		
+}
+
+
+
+// put your main code here, to run repeatedly:
+void loop() {   
+    
+  // shows sign of life  
+	float time_counter = (float)millis() * 0.001 ;	// timer_counter is in seconds
+
+	if( time_counter < 2.0 ){
+		set_point = 0 ;
+	}
+	else if(time_counter < 7.0 ){
+		set_point = 0.5 ;
+	}
+	else if(time_counter < 12.0 ){
+		set_point = 0 ;
+	}
+	else if(time_counter < 15.0 ){
+		set_point = 0 ;
+	}
+	else{
+		set_point = 0 ;
+	}
+	
+}
+
+
+//timer4 interrupt 10ms 
+ISR(TIMER4_COMPA_vect){
+
+	float time_counter = (float)millis() * 0.001 ; 		// time_counter is in seconds
+
+	char buffer_time_counter[15] = {0} ;
+	char buffer_set_point[15] = {0} ;
+    char buffer_omega[15] = {0} ;
+    char buffer_pid[15] = {0} ;
+    char buffer_odom[15] = {0} ;
+            
+  	// calculates instant speed
+    //calculate_speed(&power);
+    float theta = direc.odom * 0.00243912625 ;
+
+	float error = set_point - theta ;
+
+	float kp = 8.461 ;
+	float ki = 0.295 ;
+
+	static float integrator = 0;
+
+	// anti wind-up
+	if( abs( ( integrator + error * 0.010 ) * ki + error * kp ) < 12.0 ){
+		integrator += error * 0.010 ; // error * dt	
+	}
+	
+	float pid = error * kp + integrator * ki ;
+
+	drive_voltage( powertrain, pid );
+
+    dtostrf( time_counter, 6, 3, buffer_time_counter );
+    dtostrf( set_point, 6, 3, buffer_set_point );
+    dtostrf( theta , 6, 3, buffer_omega );
+    dtostrf( pid, 6, 3, buffer_pid );
+    dtostrf( direc.odom, 6, 3, buffer_odom );
+
+	sprintf(buffer, "[%s] : [%s] [%s] : [%s] [%s] \n", 
+		buffer_time_counter, buffer_set_point, buffer_omega, buffer_pid, buffer_odom);
+		
+  	Serial2.print( buffer );
+
+}
